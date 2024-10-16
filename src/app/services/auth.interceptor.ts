313aeb7e15be300ddd24c5a4f088f516195dfe4c
JorgeFigueroa626 <jlfigueroa626@gmail.com>
2024-10-15 22:48:23 -0400

    estructurar auth

--- src/app/services/auth.interceptor.ts
+++ src/app/services/auth.interceptor.ts
@@ -1,29 +1,38 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {
+  HttpInterceptor,
+  HttpEvent,
+  HttpHandler,
+  HttpRequest,
+  HTTP_INTERCEPTORS,
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { LoginService } from './login.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  constructor(private _loginService: LoginService) {}
 
-    constructor(private _loginService: LoginService){}
-
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        let authReq = req;
-        const token = this._loginService.getToken();
-        if (token != null) {
-            authReq = authReq.clone({setHeaders: {Authorization: `Bearer ${token}`}
-            })
-        }
-        return next.handle(authReq);
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    let authReq = req;
+    const token = this._loginService.getToken();
+    if (token != null) {
+      authReq = authReq.clone({
+        setHeaders: { Authorization: `Bearer ${token}` },
+      });
     }
+    return next.handle(authReq);
+  }
 }
 
 //lo exportamos y injectamos en el modulo principal, lo ingresamos en el provider
 export const authInterceptorProviders = [
-    {
-        provide: HTTP_INTERCEPTORS,
-        useClass : AuthInterceptor,
-        multi:true
-    }
-]
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true,
+  },
+];
