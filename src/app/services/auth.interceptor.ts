309b9a0e2c0f85b891a07f33035295eabf8cc250
JorgeFigueroa626 <jlfigueroa626@gmail.com>
2024-10-15 22:59:07 -0400

    estructurar v1

--- src/app/services/auth.interceptor.ts
+++ src/app/services/auth.interceptor.ts
@@ -28,7 +28,8 @@ export class AuthInterceptor implements HttpInterceptor {
   }
 }
 
-//lo exportamos y injectamos en el modulo principal, lo ingresamos en el provider
+//lo exportamos y injectamos en el modulo principal, 
+//lo ingresamos en el provider
 export const authInterceptorProviders = [
   {
     provide: HTTP_INTERCEPTORS,
